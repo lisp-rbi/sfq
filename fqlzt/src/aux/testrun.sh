10cd256454571d91bc4a73b4ba3807bc7eb84de0
dkorenci <damir.korencic@gmail.com>
2019-12-20 13:15:08 +0100

    improved test script - added parameters for test folder and file

--- fqlzt/src/aux/testrun.sh
+++ fqlzt/src/aux/testrun.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-#testfile=lzt\_core/test\_files/smallDict.txt
-#testfile=test-dicts/french.txt
-testfile=test-dicts/german.txt
-outfile=testdict.txt
 buildfolder=build-folder
 cliexec=lzt-cli
 
+dictfolder=$1
+dictfile=$2
+#testfile=lzt\_core/test\_files/smallDict.txt
+testfile=$dictfolder/$dictfile
+#testfile=test-dicts/german.txt
+outdict=$dictfile.lzt
+outfile=testdict.txt
+
 rm $cliexec
 cp $buildfolder/$cliexec .
+echo "testing $cliexec on $testfile"
 #chmod +x $cliexec
 # test core interface
-./$cliexec c -i $testfile -d testdict.lzt
-./$cliexec l -d testdict.lzt > $outfile
+echo "compression ..."
+./$cliexec c -i $testfile -d $outdict
+echo "list all words ..."
+./$cliexec l -d $outdict > $outfile
 cmp $testfile $outfile
 # test class interface
-./$cliexec cls -i $testfile -d testdict.lzt > $outfile
+echo "class interface ..."
+./$cliexec cls -i $testfile -d $outdict > $outfile
 cmp $testfile $outfile
+
+rm $outfile
