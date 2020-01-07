0b1ca4cc5b2b99c4c81e761eb906be1fdc9917a1
Robert Bakaric <rbakaric@exaltum.eu>
2020-01-07 13:52:34 +0100

    io_v0.02-In progress...

--- fqlzt/src/io/fastq.rs
+++ fqlzt/src/io/fastq.rs
@@ -15,3 +15,58 @@
  * and is licensed under the MIT license. For more information, see
  * <http://www.doctrine-project.org>.
  */
+
+
+
+// is there a need for a db ??
+ impl FastqaDb{
+     pub(crate) fn fastq_up<R: BufRead>(&mut self,  reader:  R ) -> Result<bool,Error> {
+
+         // check if fastq
+
+         let mut i= self.head.len();
+         let mut cnt=0;
+         self.findex.push(i);  // not sure what this is about...
+
+         for line in reader.lines() {
+             let  str = line.unwrap();
+             if &str[..1] == "@" && cnt == 0 {
+                 self.head.push(str.clone());
+                 let id = (&str[1..str.find(" ").unwrap()]).to_string();
+                 self.id.push(id.clone());
+                 self.rindex.entry(id).or_insert(Vec::new()).push(i);
+                 i=i+1;
+             }else if cnt == 1 {
+                 self.mindex.push(self.seq.len());
+                 self.seq.extend(str.as_bytes());
+             }else if cnt == 3 {
+                 self.qual.extend(str.as_bytes());
+                 cnt = 0;
+                 continue;
+             }
+             cnt = cnt+1;
+         }
+         Ok(true)
+     }
+
+     pub(crate) fn fastq_dw<W: Write> (&self, mut writer:  W)   -> Result<bool,Error>  {
+
+         for pos in self.qres.clone().into_iter() {
+             writeln!(writer, "{}", self.head[pos]).unwrap();
+             let st = self.mindex[pos];
+             let en = if pos < self.mindex.len() -1 {
+                 self.mindex[pos+1]
+             }else{
+                 self.seq.len()
+             };
+
+             writer.write_all(&self.seq[st..en]).unwrap();
+             writer.write(b"\n+\n").unwrap();
+             writer.write_all(&self.qual[st..en]).unwrap();
+             writer.write(b"\n").unwrap();
+         }
+         Ok(true)
+
+     }
+
+ }
