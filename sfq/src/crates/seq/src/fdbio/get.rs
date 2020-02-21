e3668294815c51848bc690f81789f259a0ee4ad8
Robert Bakaric <rbakaric@exaltum.eu>
2020-02-21 20:17:52 +0100

    Minor fix  get() method

--- sfq/src/crates/seq/src/fdbio/get.rs
+++ sfq/src/crates/seq/src/fdbio/get.rs
@@ -137,62 +137,89 @@ impl Get for Fdb {
         vec
     }
 
-    fn get_tsv(&self) -> Vec<u8> {
+    fn get_tsv(&self, model: &str) -> Vec<u8> {
 
-
-        let qlt = self.qual.len();
-        let len = self.head.len()+self.seq.len()+qlt+2;
-        let mut vec : Vec<u8> = vec![0u8; len];
+        let (mut hlt, mut slt, mut qlt) = (0,0,0);
         let (mut i, mut q, mut s, mut h, mut sw,  mut c) = (0,0,0,0,0,0u8);
+
+
+
+        if model.contains("q"){
+            qlt=self.qual.len();
+            sw = 2;
+        }
+        if model.contains("s"){
+            slt=self.seq.len();
+            sw = 1;
+        }
+        if model.contains("h") {
+            hlt = self.head.len();
+            sw = 0;
+        }
+
+
+        let len = slt+hlt+qlt+2;
+        let mut vec : Vec<u8> = vec![0u8; len];
 
 
         loop {
             match sw {
                 0 =>{
-                    println!(" 0");
-                    c = self.head[h];
-                    h+=1;
-                    if h == self.head.len() {
-                        vec[i] = c;
-                        i+=1;
-                        c = 10u8;
-                    }
+
+                        c = self.head[h];
+                        h+=1;
+                        if h == hlt {
+                            vec[i] = c;
+                            i+=1;
+                            if qlt > 0  || slt > 0{
+                                c = 10u8;
+                            }else{
+                                break;
+                            }
+                        }
+
                 },
                 1 => {
-                    println!(" 1");
-                    c= self.seq[s];
-                    s+=1;
-                    if s == self.seq.len() {
-                        vec[i] = c;
-                        i+=1;
-                        if qlt > 0 {
-                            c = 10u8;
-                        }else{
-                            break;
+
+                        c= self.seq[s];
+                        s+=1;
+                        if s == slt {
+                            vec[i] = c;
+                            i+=1;
+                            if qlt > 0 {
+                                c = 10u8;
+                            }else{
+                                break;
+                            }
                         }
-                    }
+
                 },
                 _ => {
-                    if qlt > 0 {
+
                         c= self.qual[q];
                         q+=1;
-                        if q == self.qual.len() {
+                        if q == qlt {
                             vec[i] = c;
                             i+=1;
                             break;
                         }
-                    }
+
                 }
             }
             if  c  == 10u8  {
-                c= '\t' as u8;
-                sw+=1;
-                if (sw == 3 && qlt > 0) || (sw == 2 && qlt == 0) {
-                    sw = 0;
+
+                if slt == 0 {sw+=2} else{sw+=1};
+                //println!("{}  -  {} {} {} {} ",c as char, sw , qlt , slt , hlt );
+                if (sw >= 3 && qlt > 0 ) || (sw == 2 && qlt == 0) {
+                    if hlt == 0 {sw = 1} else{sw=0};
                     c= 10u8;
+
+                }else{
+                    c= '\t' as u8;
                 };
 
             }
+            //println!("{},{}.", i,c as char );
             vec[i] = c;
             i+=1;
         }
