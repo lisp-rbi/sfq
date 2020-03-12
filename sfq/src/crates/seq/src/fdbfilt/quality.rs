071e4f47bc5864fdd0bdfd62f1726702979f1d5a
Robert Bakaric <rbakaric@exaltum.eu>
2020-03-12 17:09:11 +0100

    Adding filters for lossy compression: mean, med, max

---
+++ sfq/src/crates/seq/src/fdbfilt/quality.rs
@@ -0,0 +1,30 @@
+use crate::{Fdb};
+
+impl Fdb {
+
+    pub(crate) fn qavrg (&self, qual: &Vec<u8>, qavg: &mut Vec<f64>,  i: usize) {
+        for (e,q) in qual.iter().enumerate() {
+             qavg[e] = ((qavg[e] * (i-1) as f64) + *q as f64)/i as f64;
+        }
+
+    }
+    pub(crate) fn qmax (&self, qual: &Vec<u8>, qmax: &mut Vec<u8> ) {
+
+        for (e,q) in qual.iter().enumerate() {
+             if *q > qmax[e]{
+                  qmax[e] = *q;
+             }
+        }
+
+    }
+    pub(crate) fn qmin (&self, qual: &Vec<u8>, qmin: &mut Vec<u8>) {
+
+        for (e,q) in qual.iter().enumerate() {
+             if *q < qmin[e] {
+                  qmin[e] = *q;
+             }
+        }
+
+    }
+
+}
