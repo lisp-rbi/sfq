806bc1d134c377e6e6a5d7679d2cd79bc2c0ab44
Robert Bakaric <rbakaric@exaltum.eu>
2020-04-02 18:05:44 +0200

    Bug fix: wlen and alphabet size was not calcualted properly

--- sfq/src/util/common.rs
+++ sfq/src/util/common.rs
@@ -37,10 +37,11 @@ pub fn get_keys (file:&str) -> Vec<Vec<u8>>{
 }
 
 
-pub fn comp_wlen(v: &Vec<u8>, a: String) -> (usize,usize) {
+pub fn comp_wlen(v: &Vec<usize>, a: String) -> (usize,usize) {
     let alpha = a.len();
-    let cnt = v.iter().filter(|&r| *r == '\n' as u8).count() +1;
-    (cnt ,(((cnt+1)/2) as f64).log(alpha as f64).ceil() as usize)
+    let cnt = *v.iter().max().unwrap() * v.len();
+    //let cnt = v.iter().filter(|&r| *r == '\n' as u8).count() +1;
+    (cnt ,((cnt+1) as f64).log(alpha as f64).ceil() as usize)
 }
 
 
@@ -87,7 +88,7 @@ pub fn index(v: &Vec<u8>, cp: &Vec<usize>) -> (Vec<u8>, usize, usize,String) {
 
     let alpha ="ACGT".to_string(); // this should be dynamyc
 
-    let (cnt,wlen)= comp_wlen(&v, alpha.clone());
+    let (cnt,wlen)= comp_wlen(&cp, alpha.clone());
         eprintln!("Bug in common l:89");
     let mut vec = vec![0u8;v.len()+ ((cnt+1)*wlen*3)+(2*cnt)+2];
 
@@ -139,11 +140,10 @@ pub fn index(v: &Vec<u8>, cp: &Vec<usize>) -> (Vec<u8>, usize, usize,String) {
 pub fn hindex(v: &Vec<u8>, cp: &Vec<usize>) ->  (Vec<u8>, usize, usize,String)  {
 
     let alpha ="ACGT".to_string(); // this should be dynamyc
-    let (cnt,wlen)= comp_wlen(&v, alpha.clone());
+    let (cnt,wlen)= comp_wlen(&cp, alpha.clone());
 
-    eprintln!("Bug in common l:140");
-    let mut vec = vec![0u8;v.len()+ ((cnt+1)*3*wlen)+2*(cnt+2)];
-
+    eprintln!("Bug in common l:140 {} {}", cnt, wlen);
+    let mut vec = vec![0u8;v.len()*2 + ((cnt+1)*6*wlen)+2*(cnt+2)];
     let (mut x, mut y, r, f, t ) = (0,0,'A' as u8, 'G' as u8, 'X' as u8);
 
     let nvec :Vec<_> = v.split(|i| *i=='\n' as u8).collect();
@@ -255,13 +255,14 @@ pub fn tsv_encode(v: &Vec<u8>, p: bool) -> Vec<u8> {
 */
 pub fn deindex(v: &mut Vec<u8>) -> Vec<usize>  {
 
-    let (mut j, mut x, mut print, mut b) = (0,0, false,0);
+    let (mut j, mut x, mut print, mut singleton,  mut b) = (0,0, false,true, 0);
 
     let mut vec = vec![0;v.iter().filter(|&r|*r == '\n' as u8).count()+1];
 
     for (e,i) in v.clone().iter().enumerate(){
 
         if *i == 10u8 {
+            singleton = false;
             if print == true{
                 vec[x] = 1;
             }else{
@@ -286,10 +287,12 @@ pub fn deindex(v: &mut Vec<u8>) -> Vec<usize>  {
         }
     }
 
-    if print == true{
+    if singleton {eprintln!("Singleton is true");}
+
+    if print == true  || singleton == true{
         vec[x] = 1;
     }else{
-        vec[x] = decode(&v[b..v.len()].to_vec(), &b"ACGT".to_vec());
+        vec[x] = decode(&v[b..v.len()].to_vec(), &b"ACGT".to_vec())+1;
         //j+=1;
     }
 
